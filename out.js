8393d91bdc1a8966984a5336a0dbcd87c16e67bc
Jakub Szczepański <jakub.teodor.szczepanski@gmail.com>
2018-12-21 11:36:29 +0100

    JS update
    Header animation

--- out.js
+++ out.js
@@ -13,51 +13,60 @@ uk.addEventListener('click', function(){
     headerText.innerHTML = "Merry Christmas";
     headerText.classList.remove('smaller');
     headerText.classList.remove('smallerGreece');
+    headerText.classList.remove('pulse');
 });
 
 fr.addEventListener('click', function(){
     headerText.innerHTML = "Joyeux noel";
     headerText.classList.remove('smaller');
     headerText.classList.remove('smallerGreece');
+    headerText.classList.remove('pulse');
 })
 
 pl.addEventListener('click', function(){
     headerText.innerHTML = "Wesołych Świąt";
     headerText.classList.remove('smaller');
     headerText.classList.remove('smallerGreece');
+    headerText.classList.remove('pulse');
 })
 
 esp.addEventListener('click', function(){
     headerText.innerHTML = "Feliz navidad";
     headerText.classList.remove('smaller');
     headerText.classList.remove('smallerGreece');
+    headerText.classList.remove('pulse');
 })
 
 ger.addEventListener('click', function(){
     headerText.innerHTML = "Frohe Weihnachten";
     headerText.classList.add('smaller');
+    headerText.classList.remove('pulse');
 })
 
 rus.addEventListener('click', function(){
     headerText.innerHTML = "Schastlivogo Rozhdestva";
     //headerText.innerHTML = "Счастливого Рождества";
     headerText.classList.add('smaller');
+    headerText.classList.remove('pulse');
 })
 
 gr.addEventListener('click', function(){
     headerText.innerHTML = "Kalá Christoúgenna";
     //headerText.innerHTML = "Καλά Χριστούγεννα";
     headerText.classList.add('smallerGreece');
+    headerText.classList.remove('pulse');
 })
 
 it.addEventListener('click', function(){
     headerText.innerHTML = "Buon Natale";
     headerText.classList.remove('smaller');
     headerText.classList.remove('smallerGreece');
+    headerText.classList.remove('pulse');
 })
 
 swe.addEventListener('click', function(){
     headerText.innerHTML = "God jul";
     headerText.classList.remove('smaller');
     headerText.classList.remove('smallerGreece');
+    headerText.classList.remove('pulse');
 })
