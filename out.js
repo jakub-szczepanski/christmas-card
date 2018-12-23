833367ae70ed81ca64a091081a30ec2226ad0d4e
Jakub Szczepański <jakub.teodor.szczepanski@gmail.com>
2018-12-23 17:15:18 +0100

    JS update

--- out.js
+++ out.js
@@ -8,63 +8,103 @@ var gr = document.getElementById('gr');
 var it = document.getElementById('it');
 var swe = document.getElementById('swe');
 var headerText = document.getElementById('headText');
+var headMain = document.querySelector('headMain');
 
 
-uk.addEventListener('click', function(){
+function exit() {
+    $('.headMain .letters').each(function () {
+        $(this).html($(this).unwrap());
+    });
+}
+
+function movingLetters() {
+    $('.headMain .letters').each(function () {
+        $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+    });
+
+    anime.timeline({ loop: false })
+        .add({
+            targets: '.headMain .letter',
+            translateY: ["1.1em", 0],
+            translateZ: 0,
+            duration: 950,
+            delay: function (el, i) {
+                return 60 * i;
+            }
+        });
+
+}
+
+uk.addEventListener('click', function () {
     headerText.innerHTML = "Merry Christmas";
-    headerText.classList = "";
-    headerText.classList.add('fade');
+    headerText.classList.remove('pulse');
+    headerText.style.fontSize = "1.3em";
+    headerText.style.marginTop = "90px";
+    movingLetters().exit();
 });
 
-fr.addEventListener('click', function(){
+fr.addEventListener('click', function () {
     headerText.innerHTML = "Joyeux noel";
-    headerText.classList = "";
-    headerText.classList.add('rotate');
+    headerText.classList.remove('pulse');
+    headerText.style.fontSize = "1.4em";
+    headerText.style.marginTop = "90px";
+    movingLetters().exit();
 })
 
-pl.addEventListener('click', function(){
+pl.addEventListener('click', function () {
     headerText.innerHTML = "Wesołych Świąt";
-    headerText.classList = "";
-    headerText.classList.add('rotate');
+    headerText.classList.remove('pulse');
+    headerText.style.fontSize = "1.2em";
+    headerText.style.marginTop = "90px";
+    movingLetters().exit();
 })
 
-esp.addEventListener('click', function(){
+esp.addEventListener('click', function () {
     headerText.innerHTML = "Feliz navidad";
-    headerText.classList = "";
-    headerText.classList.add('rotate');
+    headerText.classList.remove('pulse');
+    headerText.style.fontSize = "1.3em";
+    headerText.style.marginTop = "90px";
+    movingLetters().exit();
 })
 
-ger.addEventListener('click', function(){
+ger.addEventListener('click', function () {
     headerText.innerHTML = "Frohe Weihnachten";
-    headerText.classList = "";
-    headerText.classList.add('smaller');
-    headerText.classList.add('rotate');
+    headerText.classList.remove('pulse');
+    headerText.style.fontSize = "1em";
+    headerText.style.marginTop = "90px";
+    movingLetters().exit();
 })
 
-rus.addEventListener('click', function(){
-    headerText.innerHTML = "Schastlivogo Rozhdestva";
-    //headerText.innerHTML = "Счастливого Рождества";
-    headerText.classList = ""
-    headerText.classList.add('smaller');
-    headerText.classList.add('rotate');
+rus.addEventListener('click', function () {
+    //headerText.innerHTML = "Schastlivogo Rozhdestva";
+    headerText.innerHTML = "Счастливого Рождества";
+    headerText.classList.remove('pulse');
+    headerText.style.fontSize = ".9em";
+    //headerText.style.marginTop = "-30px";
+    movingLetters().exit();
 })
 
-gr.addEventListener('click', function(){
-    headerText.innerHTML = "Kalá Christoúgenna";
-    //headerText.innerHTML = "Καλά Χριστούγεννα";
-    headerText.classList = ""
-    headerText.classList.add('smallerGreece');
-    headerText.classList.add('rotate');
+gr.addEventListener('click', function () {
+    //headerText.innerHTML = "Kalá Christoúgenna";
+    headerText.innerHTML = "Καλά Χριστούγεννα";
+    headerText.classList.remove('pulse');
+    headerText.style.fontSize = "1em";
+    headerText.style.marginTop = "90px";
+    movingLetters().exit();
 })
 
-it.addEventListener('click', function(){
+it.addEventListener('click', function () {
     headerText.innerHTML = "Buon Natale";
-    headerText.classList = "";
-    headerText.classList.add('rotate');
+    headerText.classList.remove('pulse');
+    headerText.style.fontSize = "1.5em";
+    headerText.style.marginTop = "90px";
+    movingLetters().exit();
 })
 
-swe.addEventListener('click', function(){
+swe.addEventListener('click', function () {
     headerText.innerHTML = "God jul";
-    headerText.classList = "";
-    headerText.classList.add('rotate');
+    headerText.classList.remove('pulse');
+    headerText.style.fontSize = "1.5em";
+    headerText.style.marginTop = "90px";
+    movingLetters().exit();
 })
